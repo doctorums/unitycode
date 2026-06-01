d5550399c2f205a965c5ae9f94ab34bf75a42684
doctorums <d_rums@icloud.com>
2026-06-02 00:49:58 +0700

    Delete book-loader.js

--- book-loader.js
+++
@@ -1,5 +0,0 @@
-async function loadBook() {
-    const response = await fetch('./data/book.json');
-    if (!response.ok) throw new Error(`Cannot load book.json (${response.status})`);
-    return await response.json();
-}
